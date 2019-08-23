8d8f3357d6a1d24934aec3fa44abd3921cf2ed2e
Daryn Luce <daryn.luce@gmail.com>
2019-08-23 00:27:54 -0500

    spotlight 2.0

---
+++ main.py
@@ -0,0 +1,21 @@
+import requests
+import datetime
+from urllib.request import urlretrieve
+import json
+import time
+
+screenWidth = '1920'
+screenHeight = '1080'
+currentCulture = 'en-US'
+currentRegion = 'us'
+utc_datetime = str(datetime.datetime.utcnow()).replace(' ','T').split('.')[0] + 'Z'
+
+def return_img(address):
+    data = json.loads(requests.get(address).content)
+    item = json.loads(data['batchrsp']['items'][0]['item'])
+    return item['ad']['image_fullscreen_001_landscape']['u']
+    
+for i in range(5,60):
+    utc_datetime = str(datetime.datetime.utcnow()).replace(' ','T').split('.')[0] + 'Z'
+    urlretrieve(return_img(f'https://arc.msn.com/v3/Delivery/Cache?pid=209567&fmt=json&rafb=0&ua=WindowsShellClient%2F0&disphorzres={screenWidth}&dispvertres={screenHeight}&lo=80217&pl={currentCulture}&lc={currentCulture}&ctry={currentRegion}&time={utc_datetime}'),f'spotlight{i}.jpg')
+    time.sleep(1)
